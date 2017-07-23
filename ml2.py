18085513113cb05a7c80a6878c9dbc906553630e
Don Jones <jonesd6000@gmail.com>
2017-07-23 09:41:32 -0700

    completed function to split data into test set and training set.

--- ml2.py
+++ ml2.py
@@ -23,3 +23,8 @@ def load_housing_data(housing_path=HOUSING_PATH):
 
 def split_train_test(data, test_ratio):
     shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
